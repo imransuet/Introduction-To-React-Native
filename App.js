ff1efcf4accfb4a6a0886af94d32e09dc763737b
imransuet <imransuet@gmail.com>
2020-11-19 22:14:15 +0100

    Button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TouchableHighlight, View,Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight, View,Image, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
 
@@ -8,19 +8,9 @@ export default function App() {
   
   return (
     <SafeAreaView style={styles.container}>
-      <Text >Open up App.</Text>
-      <TouchableHighlight onPress={()=>console.log("Image Tapped")}>
-      <Image 
-    
-    source={{
-      
-      width:200,
-      height:300,
-      uri:"https://picsum.photos/200/300"}}/>
-
-      </TouchableHighlight>
-      
-      <StatusBar style="auto" />
+      <Button 
+      color="orange"
+      title="Click Me"/>
     </SafeAreaView>
   );
 }
