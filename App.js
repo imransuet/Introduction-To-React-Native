a9f696061bfc7ed481faab45f7906f32b2252d01
imransuet <imransuet@gmail.com>
2020-11-22 07:52:37 +0100

    FlatList

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { View} from 'react-native';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return(
  
-  <ViewImageScreen/>
+  <MessagesScreen/>
 
   );
  
