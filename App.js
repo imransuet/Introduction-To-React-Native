46085aaedde289ab41365d9df82d2ce61b169c5f
imransuet <imransuet@gmail.com>
2020-11-24 18:51:47 +0100

    TextTinput

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import { View} from 'react-native';
+import React,{useState} from 'react';
+import { Text, TextInput, View} from 'react-native';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import Screen from './app/components/Screen'
-import Icon from './app/components/Icon'
-import ListItem from './app/components/ListItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
+
 export default function App() {
+const[firstName, setFirstname]=useState('');
+
   return(
- 
-   <ListingsScreen/>
+
+   <Screen>
+ <Text>{firstName}</Text>
+     <TextInput 
+     secureTextEntry={true}
+     placeholder="First Name"
+     onChangeText={text=>setFirstname(text)}
+     style={{
+
+      borderBottomColor:"#ccc",
+      borderBottomWidth:1
+     }}/>
+   </Screen>
 
   );
  
