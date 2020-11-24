3bef227e06b3e6b684652406b26f86a7324bae47
imransuet <imransuet@gmail.com>
2020-11-24 20:49:22 +0100

    Buiding Pretty TextInput

--- App.js
+++ App.js
@@ -1,27 +1,16 @@
 import React,{useState} from 'react';
-import { Text, TextInput, View} from 'react-native';
-import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
+
 import Screen from './app/components/Screen'
+import AppTextInput from './app/components/AppTextInput';
 
 export default function App() {
-const[firstName, setFirstname]=useState('');
 
   return(
 
    <Screen>
- <Text>{firstName}</Text>
-     <TextInput 
-     secureTextEntry={true}
-     placeholder="First Name"
-     onChangeText={text=>setFirstname(text)}
-     style={{
+ 
+<AppTextInput placeholder="Username" icon="email"/>
 
-      borderBottomColor:"#ccc",
-      borderBottomWidth:1
-     }}/>
    </Screen>
 
   );
